b9f8553ff5da321d9d7ea488a668eb10069cf71b
Sam Rayner <sam@samrayner.com>
2013-01-21 11:12:53 +0000

    Cache products call for filtering, test filtering and caching

--- lib/sly/interface.rb
+++ lib/sly/interface.rb
@@ -1,19 +1,49 @@
+require 'JSON'
+
 class Sly::Interface
   attr :connector
+  CACHE_DIR = File.join(File.dirname(__FILE__), '../../cache')
 
   def initialize(connector=false)
     @connector = !connector ? Sly::Connector.new : connector
   end
 
-  def products(query="")
-    products = @connector.products
+  def cache(method, query="")
+    if(!FileTest::directory?(CACHE_DIR))
+      Dir::mkdir(CACHE_DIR)
+    end
 
+    cache_file = "#{CACHE_DIR}/#{method}.json"
+    items = []
+
+    if(query.empty? || !File.exists?(cache_file))
+      items = @connector.send(method.to_sym)
+      File.open(cache_file, 'w') do |f|  
+        f.puts items.to_json
+      end
+    else
+      begin
+        File.open(cache_file, 'r') do |f|  
+          items = JSON(f.read)
+        end
+      rescue
+        items = @connector.send(method.to_sym)
+      end
+    end
+
+    items
+  end
+
+  def products(query="")
+    products = self.cache("products", query)
+
+    #JSON error message returned
     if(!products.kind_of? Array)
       return []
     end
 
     #filter by query
-    products = products.select { |product| query.empty? or product["name"].downcase.include?(query.downcase) }
+    products = products.select { |product| query.empty? || product["name"].downcase.include?(query.downcase) }
 
     products.map { |product| Sly::Product.new(product) }
   end
--- spec/sly/interface_spec.rb
+++ spec/sly/interface_spec.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 describe Sly::Interface, integration: true do
   before :all do
     good_config = Sly::Config.new({email:ENV["sprintly_email"], api_key:ENV['sprintly_api_key'], product_id:ENV['sprintly_product_id']})
@@ -6,6 +8,36 @@ describe Sly::Interface, integration: true do
 
     @api = Sly::Interface.new(Sly::Connector.new(good_config))
     @bad_api = Sly::Interface.new(Sly::Connector.new(bad_config))
+
+    CACHE_DIR = File.join(File.dirname(__FILE__), '../../cache')
+  end
+
+  describe :cache do 
+    it "creates a cache directory if one does not exist" do
+      if(FileTest::directory?(CACHE_DIR))
+        FileUtils.rm_rf(CACHE_DIR)
+      end
+
+      FileTest::directory?(CACHE_DIR).should be_false
+
+      @api.cache("products")
+
+      FileTest::directory?(CACHE_DIR).should be_true
+    end
+
+    it "creates a cache file if one does not exist" do
+      cache_file = "#{CACHE_DIR}/products.json"
+
+      if(File.exists?(cache_file))
+        File.delete(cache_file)
+      end
+
+      File.exists?(cache_file).should be_false
+
+      @api.cache("products")
+
+      File.exists?(cache_file).should be_true
+    end
   end
 
   describe :products do
@@ -17,6 +49,19 @@ describe Sly::Interface, integration: true do
     it "returns an empty list with a bad config" do
       @bad_api.products.should be_a_kind_of(Array)
       @bad_api.products.should be_empty
+    end
+
+    it "filters products by name" do
+      filter = "e"
+      filtered = @api.products(filter)
+      filtered.should be_a_kind_of(Array)
+
+      filtered.each do |product|
+        product.name.downcase.should include(filter)
+      end
+
+      @api.products("xxxxxx").should be_a_kind_of(Array)
+      @api.products("xxxxxx").should be_empty
     end
   end
 
