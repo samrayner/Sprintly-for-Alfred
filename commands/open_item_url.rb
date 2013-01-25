2f8216672a8224b816c7e0d80230295a2dad9614
Sam Rayner <sam@samrayner.com>
2013-01-25 18:22:27 +0000

    Open item URL on alt-return when listing items

---
+++ commands/open_item_url.rb
@@ -0,0 +1,7 @@
+QUERY = ARGV[0].to_s.downcase.strip
+require_relative "../lib/sly"
+
+sly = Sly::Interface.new
+product_id = sly.connector.config.product_id
+url = "https://sprint.ly/product/#{product_id}/#!/item/#{QUERY}"
+`open #{url}`
